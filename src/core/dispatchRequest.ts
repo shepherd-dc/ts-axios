a09373475af63eb274e9e0010ccd812391c76ec4
shepherd_dc <shepherd_dc@aliyun.com>
2020-12-04 16:45:41 +0800

    feat(core extend): axios basic apis
    axios basic apis by mixed Object

---
+++ src/core/dispatchRequest.ts
@@ -0,0 +1,38 @@
+import { transformRequest, transformResponse } from '../helpers/data'
+import { processHeaders } from '../helpers/headers'
+import { bulidURL } from '../helpers/url'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types/index'
+import xhr from './xhr'
+
+export default function dispatchRequest(config: AxiosRequestConfig): AxiosPromise {
+  processConfig(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
+  // console.log(config.headers)
+  config.data = transformRequestData(config)
+}
+
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return bulidURL(url!, params)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
+}
